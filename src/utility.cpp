0e2ee31f99876280d7b7f7d555b087882fdab11e
LordAcrobaticTurtle <z5160890@ad.unsw.edu.au>
2023-01-22 16:17:01 +1100

    Changed utility to use pointer arithmetic to avoid need to pass arrays

--- src/utility.cpp
+++ src/utility.cpp
@@ -1,16 +1,36 @@
 #include <utility.h>
 
-int i2c_write(I2CDriverWire * p_wirePtr, int p_deviceAddress, unsigned char * p_registers, unsigned int p_length, bool p_stop) {
+int i2c_write(
+    I2CDriverWire * p_wirePtr, 
+    int p_deviceAddress, 
+    unsigned char * p_registers, 
+    unsigned int p_length, 
+    bool p_stop
+) {
     
     // Safety programming
     p_wirePtr->beginTransmission(p_deviceAddress);    
-    for (unsigned int i = 0; i < p_length; i++) 
-        p_wirePtr->write(p_registers[i]);
+    // unsigned char * ptr = p_registers;
+    // ptr != p_registers + p_length
+    // ptr++
+
+    // for (unsigned int i = 0; i < p_length; i++) 
+    //     p_wirePtr->write(p_registers[i]);
+    for (unsigned char * ptr = p_registers; ptr != p_registers + p_length; ptr++) {
+        p_wirePtr->write(*ptr);
+    }
+
     // Error checking
     return p_wirePtr->endTransmission(p_stop);
 }
 
-int i2c_read(I2CDriverWire * p_wirePtr, int p_address, unsigned int p_numBytesRequested, unsigned char * p_buffer, unsigned int p_bufferSize) {
+int i2c_read(
+    I2CDriverWire * p_wirePtr, 
+    int p_address, 
+    unsigned int p_numBytesRequested, 
+    unsigned char * p_buffer, 
+    unsigned int p_bufferSize
+) {
     // Safe programming
     if (p_bufferSize < p_numBytesRequested) 
         return -1;
