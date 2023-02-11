5626d1500f94eaa1238f2013323be57561ed3412
LordAcrobaticTurtle <z5160890@ad.unsw.edu.au>
2023-02-11 23:24:50 +1100

    Updated main to use as IMU test bed.

--- src/main.cpp
+++ src/main.cpp
@@ -4,32 +4,37 @@
 #include <RadioInterface.h>
 #include <Rosbot.h>
 #include <AS5600.h>
+#include <IMU.h>
 #include <i2c_device.h>
 #include <i2c_driver.h>
 
-Rosbot robot;
+IMU imu(&Wire1);
+// Rosbot robot;
 
 void setup()
 {  
     // Setup I2C comms
     Wire.begin();
+    // Wire.setClock(400000);
     Wire1.begin();
 
     // Setup serial lines
     Serial.begin(115200);
     Serial.println("Begin!");
-
+ 
     // Switch on LED
     pinMode(LED_BUILTIN, OUTPUT);
     digitalWrite(LED_BUILTIN, HIGH);
-
+    
     // Setup rosbot
-    robot.setup();    
+    // robot.setup();    
+    imu.setup();
 }
 
 void loop()
 {
-    robot.tankDrive();
+    // robot.tankDrive();
+    imu.update();
 }
 
 
