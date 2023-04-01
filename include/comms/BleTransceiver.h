82abf14e89665e0cb5cab1b2a45540081478f03f
LordAcrobaticTurtle <z5160890@ad.unsw.edu.au>
2023-04-01 21:02:59 +1100

    Basic interface

--- include/comms/BleTransceiver.h
+++ include/comms/BleTransceiver.h
@@ -1,16 +1,20 @@
 #pragma once
 #include <Arduino.h>
 
+#define ENOSPC -2
+#define EARG -1
+
+
+
 class BLETransceiver {
     public:
         BLETransceiver(HardwareSerial * hwSerial);
         
         void configure();
 
-        void readBytes(byte * buffer, unsigned int bufferLength);
-        void sendBytes(byte * buffer, unsigned int bufferLength);
+        int readBytes(byte * buffer, uint32_t bufferLength);
+        int sendBytes(byte * buffer, uint32_t bufferLength);
 
     private:
-        
         HardwareSerial *m_hwSerial;
 };
--- src/comms/BleTransceiver.cpp
+++ src/comms/BleTransceiver.cpp
@@ -4,3 +4,27 @@
 BLETransceiver::BLETransceiver(HardwareSerial *hwSerial) {
     m_hwSerial = hwSerial;
 }
+
+int BLETransceiver::readBytes(byte * buffer, uint32_t bufferLength) {
+    
+    if (m_hwSerial == NULL) {return -1;}
+
+    if (m_hwSerial->available()) {
+        m_hwSerial->readBytes(buffer, bufferLength);
+    }
+
+    return 0;
+}
+
+int BLETransceiver::sendBytes(byte *buffer, uint32_t bufferLength) {
+    if (buffer == NULL) return EARG;
+    if (bufferLength <= 0) return EARG;
+
+    if (m_hwSerial->availableForWrite() >= bufferLength) {
+        int error = m_hwSerial->write(buffer, bufferLength);
+    } else {
+        return ENOSPC;
+    }
+
+    return 0;
+}
