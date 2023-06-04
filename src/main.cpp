3beaa042d4577c7e795b9784a832303572d5af45
Sam Hall <s.hall@reachrobotics.com>
2023-06-04 11:35:59 +1000

    Added led code to indicate when IMU is ready

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,26 @@
 
 #include <AccelStepper.h>
 #include <MultiStepper.h>
-// #include <Rosbot.h>
+#include <Rosbot.h>
 #include <drivers/RadioInterface.h>
 #include <drivers/AS5600.h>
 #include <drivers/IMU.h>
 #include <drivers/RGB_LED.h>
 #include <drivers/DRV8876.h>
 #include <utility/math.h>
-#include <i2c_device.h>
-#include <i2c_driver.h>
 
 // Rosbot robot;
 IMU imu;
+RGBLED led(3,4,2,true);
 
 void setup()
 {  
     Serial.begin(115200);
     Serial.println("Begin!");
     Master.begin(100 * 1000U);
+    led.switchRedOn();    
     imu.setup(Master);
+    led.switchGreenOn();
     // robot.setup();
     pinMode(LED_BUILTIN, OUTPUT);
     digitalWrite(LED_BUILTIN, HIGH);
@@ -28,5 +29,6 @@ void setup()
 void loop()
 {
     // robot.update();
+    imu.update();
 }
 
