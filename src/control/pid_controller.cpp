5f36429db690f199747f9f566414daac524ff7f7
Sam Hall <s.hall@reachrobotics.com>
2023-10-06 16:42:55 +1100

    Added PID controller definition

--- src/control/pid_controller.cpp
+++ src/control/pid_controller.cpp
@@ -1,10 +1,14 @@
 #include "control/pid_controller.h"
+#include <Arduino.h>
 
 float PIDController::computeResponse(PIDParams &params) {
     float error = params.currValue - params.target;
     float derror = (error - params.prevError)/params.dt;
     params.errorSum += error*params.dt;
     float response = params.kp * error + params.kd * derror + params.ki * params.errorSum;
+    response = constrain(response, -1, 1);
+    params.errorSum = constrain(params.errorSum, -1, 1);
     params.prevError = error;
+    // Serial.println("ErrorSum = " + String(params.errorSum) + ", i term" + String(params.ki*params.errorSum));
     return response;
 }
