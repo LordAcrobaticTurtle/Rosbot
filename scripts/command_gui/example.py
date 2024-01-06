29b0c2d36918de9272bd3da79783fff44f440c2a
Sam Hall <s.hall@reachrobotics.com>
2024-01-06 16:58:59 +1100

    Don't work. it garbage

--- scripts/command_gui/example.py
+++ scripts/command_gui/example.py
@@ -1,29 +1,44 @@
-import matplotlib.pyplot as plt
-import time
-import random
- 
-ysample = random.sample(range(-50, 50), 100)
- 
-xdata = []
-ydata = []
- 
-plt.show()
- 
-axes = plt.gca()
-axes.set_xlim(0, 50)
-axes.set_ylim(-50, +50)
-line, = axes.plot(xdata, ydata, 'r-')
- 
-for i in range(100):
-    xdata.append(i)
-    ydata.append(ysample[i])
-    line.set_xdata(xdata)
-    line.set_ydata(ydata)
-    if (i > 10):
-        axes.set_xlim(i-10, i+10)
-    plt.draw()
-    plt.pause(1e-17)
-    time.sleep(0.001)
- 
-# add this if you don't want the window to disappear at the end
-# plt.show()
+# Import tkinter in the notebook
+from tkinter import *
+
+# Create an instance of window of frame
+win = Tk()
+
+# set Title
+win.title('Toggle Button Demonstration')
+
+# Set the Geometry
+win.geometry("700x400")
+win.resizable(0, 0)
+
+# Create a variable to turn on the button initially
+is_on = True
+
+# Create Label to display the message
+label = Label(win, text="Night Mode is On", bg="white", fg="black", font=("Poppins bold", 22))
+label.pack(pady=20)
+
+# Define our switch function
+def button_mode():
+   global is_on
+
+   # Determine it is on or off
+   if is_on:
+      on_.config(image=off)
+      label.config(text="Day Mode is On", bg="white", fg="black")
+      is_on = False
+   else:
+      on_.config(image=on)
+      label.config(text="Night Mode is On", fg="black")
+      is_on = True
+
+# Define Our Images
+on = PhotoImage(file="record-button.png")
+off = PhotoImage(file="stop-button.png")
+
+# Create A Button
+on_ = Button(win, bd=0, command=button_mode)
+on_.pack(pady=50)
+
+# Keep Running the window
+win.mainloop()
