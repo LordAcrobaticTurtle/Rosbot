00c7bb25573417fef4b368598f23077ed66dc21e
Sam_Hall_bpl <s.hall@blueprintlab.com>
2024-02-12 08:16:05 +1100

    Removed model dependency

--- scripts/Mech_Mate/controllers/pid_tuning_handler.py
+++ scripts/Mech_Mate/controllers/pid_tuning_handler.py
@@ -2,17 +2,16 @@
 from model import Model
 from view import View
 from controllers.serial_port_handler import SerialPortHandler
-from ui_elements.ui_pid_tuning import PIDTuningCallbackIndex
+from ui_elements.ui_pid_tuning import PIDTuningCallbackIndex, PidTuning
 
 class PIDTuningHandler():
 
-    def __init__(self, ui : View, data : Model, robotConnection : SerialPortHandler) -> None:
+    def __init__(self, ui : PidTuning, robotConnection : SerialPortHandler) -> None:
         self._ui = ui
-        self._model = data
         self._serialConnection = robotConnection
 
     def populate_callbacks(self):
-        self._ui._pidTuner._callbacks[PIDTuningCallbackIndex]["Send"] = self.send
+        self._ui._callbacks[PIDTuningCallbackIndex]["Send"] = self.send
         
     def send(self, p : float, i : float, d : float):
         buffer = f"Set-pid [{p},{i},{d}]"
