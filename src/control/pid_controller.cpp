d277af95bd8e5474366c0c1c621358079f75ca1b
Sam Hall <s.hall@reachrobotics.com>
2024-03-30 13:31:17 +1100

    Added output to struct and double precision

--- src/control/pid_controller.cpp
+++ src/control/pid_controller.cpp
@@ -2,15 +2,15 @@
 #include <Arduino.h>
 
 float PIDController::computeResponse(PIDParams &params) {
-    float error = params.currValue - params.target;
-    float derror = (error - params.prevError)/params.dt;
+    double error = params.currValue - params.target;
+    double derror = (error - params.prevError)/params.dt;
     params.errorSum += error*params.dt;
-    float response = params.kp * error + params.kd * derror + params.ki * params.errorSum;
-    response = constrain(response, params.bounds[0], params.bounds[1]);
+    params.response = params.kp * error + params.kd * derror + params.ki * params.errorSum;
+    // response = constrain(response, params.bounds[0], params.bounds[1]);
     params.errorSum = constrain(params.errorSum, params.bounds[0], params.bounds[1]);
     params.prevError = error;
     // Serial.println("ErrorSum = " + String(params.errorSum) + ", i term" + String(params.ki*params.errorSum));
-    return response;
+    return params.response;
 }
 
 // template<typename T>
