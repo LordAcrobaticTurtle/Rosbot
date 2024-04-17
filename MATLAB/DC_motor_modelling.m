5d9c358d7811d038ebc9e57771ec5f23643bda4c
Sam Hall <s.hall@blueprintlab.com>
2024-04-18 08:06:36 +1000

    Added matrix stuff to file

--- MATLAB/DC_motor_modelling.m
+++ MATLAB/DC_motor_modelling.m
@@ -24,3 +24,13 @@ step(sys_cl,t)
 grid
 title('Step Response with Proportional Control')
          
+A = [-b/J   K/J
+    -K/L   -R/L];
+B = [0
+    1/L];
+C = [1   0];
+D = 0;
+motor_ss = ss(A,B,C,D);
+
+
+
