2e34c9aed687f53857665f0d362d3fb6e1fac43e
Sam Hall <s.hall@blueprintlab.com>
2024-04-18 08:06:12 +1000

    Added half a speed controller

---
+++ include/control/speed_control.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <interfaces/interfaces.h>
+
+
+class MotorSpeedControl {
+
+    // MotorSpeedControl () = default;
+    // ~MotorSpeedControl () = default;
+    
+    static float calculatedOutputVoltage (float current, float rotationSpeed);
+
+    
+
+
+    
+};
