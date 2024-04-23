183dbc3e689a1cdab78905b7d3ab7efb57951335
Sam Hall <s.hall@blueprintlab.com>
2024-04-24 09:05:01 +1000

    Changed physical variables

--- MATLAB/DC_motor_modelling.m
+++ MATLAB/DC_motor_modelling.m
@@ -1,10 +1,10 @@
 %% DC motor modelling 
 % syms J b K R L;
-J = 0.01;
+J = 0.0000045;
 b = 0.1;
-K = 0.01;
-R = 1;
-L = 0.5;
+K = 0.35;
+R = 12.3;
+L = 0.004;
 s = tf('s');
 P_motor = K/((J*s+b)*(L*s+R)+K^2);
 
@@ -32,5 +32,23 @@ C = [1   0];
 D = 0;
 motor_ss = ss(A,B,C,D);
 
+poles = eig(A)
+
+Q = C'*C;
+R = 1;
+K = lqr(A,B,Q,R)
 
 
+states = {'dtheta' 'current'};
+inputs = {'V'};
+outputs = {'dtheta'};
+
+sys_cl = ss(Ac,Bc,Cc,Dc,'statename',states,'inputname',inputs,'outputname',outputs);
+
+t = 0:0.01:5;
+r =0.2*ones(size(t));
+[y,t,x]=lsim(sys_cl,r,t);
+[AX,H1,H2] = plotyy(t,y(:,1),t,y(:,2),'plot');
+set(get(AX(1),'Ylabel'),'String','dtheta (m)')
+% set(get(AX(2),'Ylabel'),'String','pendulum angle (radians)')
+title('Step Response with LQR Control')
